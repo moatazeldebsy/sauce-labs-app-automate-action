6838e862fa66f51675f1127cb74cdf6bd93ba516
moatazeldebsy <moataznabil2019@gmail.com>
2020-03-15 22:14:01 +0100

    change files

--- saucelabs-action/entrypoint.sh
+++ saucelabs-action/entrypoint.sh
@@ -1,13 +1,2 @@
 #!/bin/sh -l
 echo "Hello $1"
-echo "Downloading Saucelabs Runner for Virtual Devices"
-
-wget https://saucelabs.com/downloads/sauce-runner-virtual-0.1.1-linux.zip
-
-unzip sauce-runner-virtual-0.1.1-linux.zip
-
-echo "starting uploading app and test apk to Saucelabs and run the automated tests"
-
-  ./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
-  -f espresso -a $1 \
-  -t $2 -d "deviceName=$3" --data-center $4
