b3da51aef04d73268ef6a550e27a49b957020233
moatazeldebsy <moataznabil2019@gmail.com>
2020-03-15 21:53:43 +0100

    change files

--- saucelabs-action/entrypoint.sh
+++ saucelabs-action/entrypoint.sh
@@ -9,5 +9,5 @@ unzip sauce-runner-virtual-0.1.1-linux.zip
 echo "starting uploading app and test apk to Saucelabs and run the automated tests"
 
   ./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
-  -f espresso -a $apk_path \
-  -t $test_apk_path -d "deviceName=${device_name}" --data-center ${data_center}
+  -f espresso -a $1 \
+  -t $2 -d "deviceName=$3" --data-center $4
