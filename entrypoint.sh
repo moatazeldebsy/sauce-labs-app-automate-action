4a9eca63737c1964e81751e4c200d6e674b8ac70
moatazeldebsy <moataznabil2019@gmail.com>
2020-03-16 11:22:48 +0100

    update sauce runner version

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 set -ex
 echo "Downloading Saucelabs Runner for Virtual Devices"
 
-wget https://saucelabs.com/downloads/sauce-runner-virtual-0.1.1-linux.zip
+wget https://saucelabs.com/downloads/sauce-runner-virtual-0.1.2-linux.zip
 
-unzip sauce-runner-virtual-0.1.1-linux.zip
+unzip sauce-runner-virtual-0.1.2-linux.zip
 
 echo "starting uploading app and test apk to Saucelabs and run the automated tests"
 
-  ./sauce-runner-virtual-0.1.1-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
+  ./sauce-runner-virtual-0.1.2-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
   -f espresso -a $1 \
   -t $2 -d "deviceName=$3" --data-center $4
