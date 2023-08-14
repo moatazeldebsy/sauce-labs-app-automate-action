7d48f189f27c69f00c3568a7d53fe307057e4ee2
Sharecare-NGurram <124046895+Sharecare-NGurram@users.noreply.github.com>
2023-08-14 16:19:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ echo "starting uploading app and test apk to Saucelabs and run the automated tes
 
   ./sauce-runner-virtual-0.1.2-linux/bin/sauce-runner-virtual  -u $SAUCELABS_USERNAME  -k $SAUCELABS_ACCESS_KEY \
   -f espresso -a $1 \
-  -t $2 -d "deviceName=$3" --data-center $4
+  -a $2 -d "deviceName=$3" --data-center $4
